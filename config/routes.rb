a0f17eeffb6f6281c2b275a9057eb0139595c473
Nancy Kayitare <kayitarenancy@gmail.com>
2019-04-02 11:18:23 +0200

    created routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :bookings do
+    resource :hotels, :cars, :guides, :attractions, only: [:show]
+  end
+
+  resource :hotels
+  resource :cars
+  resource :attractions
+  resource :guides
 end
